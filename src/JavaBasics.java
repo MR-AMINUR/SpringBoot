c4b68225a55cd6ea6e54b7509d88347955420aa6
AMINUR <aminur01mallick@gmail.com>
2026-06-22 23:26:20 +0530

    java file added

--- src/JavaBasics.java
+++ src/JavaBasics.java
@@ -1,2 +1,7 @@
 public class JavaBasics {
+
+    static void main() {
+        System.out.println("Github Connection Configuration..");
+        System.out.println("Github Connection Configuration..");
+    }
 }
